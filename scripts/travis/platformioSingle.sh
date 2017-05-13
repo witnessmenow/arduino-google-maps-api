f5730f7bdc62f62c9cbdb4ad72ac8919590d14da
witnessmenow <ecenuig@gmail.com>
2017-05-13 04:26:34 +0100

    going to fail

--- scripts/travis/platformioSingle.sh
+++ scripts/travis/platformioSingle.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -eux
 
-platformio ci $PWD/examples/$BOARDTYPE$EXAMPLE_FOLDER$EXAMPLE_NAME/$EXAMPLE_NAME.ino -l '.' -l '/home/travis/.platformio/lib/ArduinoJson_ID64' -l '/home/travis/.platformio/lib/JsonStreamingParser_ID561' -b $BOARD
+platformio ci $PWD/examples/$BOARDTYPE$EXAMPLE_FOLDER$EXAMPLE_NAME/$EXAMPLE_NAME.ino -l '.GoogleMapsApi.h' -b $BOARD
